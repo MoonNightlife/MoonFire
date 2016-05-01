ffdc307cb11e2bd588499c9a44b27a63ca7131a1
Evan Noble <evannoble@moonnightlife.com>
2016-04-30 23:38:22 -0400

    Profile Pic
    added a profile picture to the user. the user can now tap on the image
    view and select a photo off their phone

--- Moon/ProfileViewController.swift
+++ Moon/ProfileViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import Firebase
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let tapPic = UITapGestureRecognizer()
     
     // MARK: - Outlets
 
@@ -24,6 +26,18 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //sets a circular profile pic
+        profilePicture.layer.borderWidth = 1.0
+        profilePicture.layer.masksToBounds = false
+        profilePicture.layer.borderColor = UIColor.whiteColor().CGColor
+        profilePicture.layer.cornerRadius = profilePicture.frame.size.height/2
+        profilePicture.clipsToBounds = true
+
+        // Adds tap gesture
+        tapPic.addTarget(self, action: #selector(ProfileViewController.tappedProfilePic))
+        profilePicture.addGestureRecognizer(tapPic)
+        profilePicture.userInteractionEnabled = true
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -37,9 +51,42 @@ class ProfileViewController: UIViewController {
             self.email.text = snap.value["email"] as? String
             self.age.text = snap.value["age"] as? String
             self.gender.text = snap.value["gender"] as? String
+            
+            let base64EncodedString = snap.value["profilePicture"]
+            if let imageString = base64EncodedString! {
+                let imageData = NSData(base64EncodedString: imageString as! String, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
+                let decodedImage = UIImage(data:imageData!)
+                self.profilePicture.image = decodedImage
+            }
+            
             }) { (error) in
                 print(error.description)
         }
+        
+        
+    }
+    
+    // Displays the photo library after the user taps on the profile picture
+    func tappedProfilePic(){
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+    }
+    
+    // Sets the photo in the view and firebase after a photo is selected
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        profilePicture.image = image
+        
+        // Save image to firebase
+        let imageData = UIImageJPEGRepresentation(image,0.1)
+        let base64String = imageData?.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+        currentUser.childByAppendingPath("profilePicture").setValue(base64String)
     }
 
 }
